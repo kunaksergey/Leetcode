828917825129bc34e58209dca8650961460adbf9
kunaksergey <kunaksergey@ukr.net>
2020-11-12 21:29:08 +0200

    task 85

--- src/main/java/com/home/task85/Task85Solution.java
+++ src/main/java/com/home/task85/Task85Solution.java
@@ -3,12 +3,15 @@ package com.home.task85;
 // 85. Maximal Rectangle
 public class Task85Solution {
     public int maximalRectangle(char[][] matrix) {
+        if(matrix.length==0 || matrix[0].length==0) return 0;
+
         int maximalRectangle =0;
 
         for(int i = 0; i <matrix.length; i++) {
             for (int j = 0; j <matrix[i].length ; j++) {
             if(matrix[i][j]==1){
-                int countRectangle = countRectangle(i,j);
+                int [] borderRectangle = borderRectangle(i,j, matrix);
+                int countRectangle = countRectangle(borderRectangle, matrix);
                 maximalRectangle = Math.max(maximalRectangle,countRectangle);
             }
             }
@@ -17,7 +20,30 @@ public class Task85Solution {
         return maximalRectangle;
     }
 
-    private int countRectangle(int i, int j) {
-        return 1;
+    private int[] borderRectangle(int startX, int startY, char[][] matrix) {
+        if(matrix.length==0 || matrix[0].length==0) return new int[]{0,0,0,0};
+
+        int endX = matrix.length;
+        int endY = matrix[0].length;
+
+        int stopX = startX;
+        while((matrix[stopX][startY]==1) && stopX<matrix.length){
+            stopX++;
+        }
+
+        int stopY = startY;
+        while((matrix[startX] [stopY]==1) && stopY<matrix[0].length){
+            stopY++;
+        }
+
+        return new int[]{startX,stopX,startY,stopY};
+    }
+
+
+    private int countRectangle(int[] border, char[][] matrix){
+        int count = 0;
+        if(border.length!=4) return 0;
+
+        return count;
     }
 }
