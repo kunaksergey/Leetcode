f4a8d39c0278467699caced77376adc5210ed62f
Serhii Kunak <Serhii.Kunak@halliburton.com>
2020-11-12 10:50:16 +0200

    add Task 114

---
+++ src/main/java/com/home/task114/Task114Solution.java
@@ -0,0 +1,8 @@
+package com.home.task114;
+
+import com.home.core.TreeNode;
+
+public class Task114Solution {
+    public void flatten(TreeNode root) {
+    }
+}
---
+++ src/test/java/com/home/task114/Task114SolutionTest.java
@@ -0,0 +1,39 @@
+package com.home.task114;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Task114SolutionTest {
+    private Task114Solution solution;
+
+    @Before
+    public void before(){
+        this.solution = new Task114Solution();
+    }
+
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+    }
+
+    @Test
+    public void test4() {
+
+    }
+
+
+}
