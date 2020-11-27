77d46deb91b22d7f2cd2815c8bc637ecdcb5e6cd
Serhii Kunak <Serhii.Kunak@halliburton.com>
2020-11-27 15:24:17 +0200

    Task worked version

--- src/main/java/com/home/task76/Task76Solution.java
+++ src/main/java/com/home/task76/Task76Solution.java
@@ -1,128 +1,185 @@
 package com.home.task76;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Task76Solution {
-    public static void main(String[] args) {
-        List<Integer> list =  new ArrayList<>(400000);
 
-        long start = System.currentTimeMillis();
-        for (int i = 0; i <400000 ; i++) {
-            list.add(i);
-        }
+    final int[] deff = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE};
+    final int maxlength = 100000;
+    final String emptyValue = "";
+    int[] minRange = deff;
 
-        System.err.println(System.currentTimeMillis()-start);
-
-         list =  new ArrayList<>();
-         start = System.currentTimeMillis();
-
-        for (int i = 0; i <400000 ; i++) {
-            list.add(i);
-        }
-        System.err.println(System.currentTimeMillis()-start);
-    }
     public String minWindow(String s, String t) {
-        int [] deff = new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
-
-        if(t.length()>s.length() || !containsAll(s,t)) {
-            return "";
+        if (t.length() > s.length() || !containsAll(s, t) || t.length() > maxlength || s.length() > maxlength) {
+            return emptyValue;
         }
 
-      //  s = trim(s,t);
-        long start = System.currentTimeMillis();
-        int[] startEndIndex=  findWindow(s, 0, s.length()-1, t, deff);
-        if(startEndIndex==deff){
-            return "";
+        WordMapStore wordMapStore = new WordMapStore(t);
+
+        int[] windowsRange = findWindow(s, 0, s.length() - 1, wordMapStore);
+
+        //no solution
+        if (windowsRange[0] == deff[0] || windowsRange[1]==deff[1]) {
+            return emptyValue;
         }
-         startEndIndex= trimRange(s, startEndIndex[0], startEndIndex[1], t, deff, true, false);
-        int startIndex = startEndIndex[0];
-        int endIndex = startEndIndex[1];
 
-        int[] minRange = startEndIndex;
-
-        if(startIndex == Integer.MAX_VALUE && endIndex==Integer.MIN_VALUE) {
-            return "";
+        // optimal solution
+        if ((windowsRange[1] - windowsRange[0]) == (t.length() - 1)) {
+            return s.substring(windowsRange[0], windowsRange[1] + 1);
         }
-        System.err.println(System.currentTimeMillis() - start);
-         start = System.currentTimeMillis();
 
-        if((minRange[1]-minRange[0])==(t.length()-1)) {
-            return s.substring(minRange[0],minRange[1]+1);
-        }
-        int count = 0;
 
-        char rightLetter= s.charAt(startIndex);
+        // save first found range as min
+        minRange = windowsRange;
+
+        // startEndIndex= trimRange(s, startEndIndex[0], startEndIndex[1], t, deff, true, false);
+        int startIndex = windowsRange[0];
+        int endIndex = windowsRange[1];
+
+
+
         //move window
+        boolean moveToNext = true;
 
-        startIndex = startIndex + 1;
-        endIndex = Math.min(endIndex + 1, s.length()-1);
+        while (moveToNext && (canMoveLeft(startIndex+1, s, t) || canMoveRight(endIndex+1, s))) {
+            char letterToFind = s.charAt(startIndex);
+         
+            // move right side
+            boolean movedRight = false;
+            while (canMoveRight(endIndex+1, s)) {
+                endIndex++;
+                char currentChar = s.charAt(endIndex);
 
-        while(startIndex<=s.length()-t.length() && endIndex<s.length()) {
-            while( endIndex<s.length() && s.charAt(endIndex)!= rightLetter) {
-                endIndex = endIndex + 1;
+                //save all chars that has word till right letter
+                if(currentChar != letterToFind) {
+                    if (wordMapStore.has(currentChar)) {
+                        wordMapStore.add(currentChar);
+                    }
+                }else{
+                    if (wordMapStore.has(currentChar)) {
+                        wordMapStore.add(currentChar);
+                        movedRight = true;
+                     break;
+                    }
+                }
+ }
+
+            //move left side
+
+            boolean removedLeft = false;
+             if(wordMapStore.remove(s.charAt(startIndex))){
+                 removedLeft = true;
+             };
+
+            while (removedLeft && canMoveLeft(startIndex+1, s, t)) {
+                startIndex++;
+                char leftChar = s.charAt(startIndex);
+                if (wordMapStore.has(leftChar)) {
+                    boolean removed = wordMapStore.remove(leftChar);
+                    if (!removed) { // i.e
+                       break;
+                    }else{
+                        removedLeft = true;
+                    }
+                } else {
+                    // nothing;
+                }
             }
-            if(endIndex>=s.length()){
+
+
+
+            if(removedLeft || movedRight) {
+                updateMinRange(new int[]{startIndex, Math.min(endIndex, s.length() - 1)});
+            }
+
+            moveToNext = removedLeft || movedRight;
+           }
+        return s.substring(minRange[0], minRange[1] + 1);
+    }
+
+    private boolean canMoveLeft(int leftIndex, String s, String t) {
+        return leftIndex <= s.length() - t.length();
+    }
+
+    private boolean canMoveRight(int rightIndex, String s) {
+        return rightIndex < s.length();
+    }
+
+    public void updateMinRange(int[] range) {
+        if ((range[1] - range[0]) < (minRange[1] - minRange[0])) {
+            minRange[0] = range[0];
+            minRange[1] = range[1];
+        }
+    }
+
+    public int[] findWindow(String s, int startIndex, int endIndex, WordMapStore wordMapStore) {
+        int leftIndex = Integer.MAX_VALUE;
+        int rightIndex = Integer.MIN_VALUE;
+        for (int i = startIndex; i <= endIndex; i++) {
+            char c = s.charAt(i);
+            if(leftIndex == Integer.MAX_VALUE && wordMapStore.has(c)){
+                leftIndex=i; // found left board
+            }
+            wordMapStore.add(c);
+            if (wordMapStore.isFull()) {
+                rightIndex = i;
                 break;
             }
-            int[] nextStartEndIndex = trimRange(s,startIndex,  endIndex, t, deff, true, false);
-
-            if(startIndex==nextStartEndIndex[0] && endIndex==nextStartEndIndex[1]){
-                rightLetter = s.charAt(startIndex);
-                endIndex++;
-                continue;
-            }
-            if(nextStartEndIndex[0] == Integer.MAX_VALUE && nextStartEndIndex[1]==Integer.MIN_VALUE) {
-               continue;
-            }
-            if((nextStartEndIndex[1]-nextStartEndIndex[0])==(t.length()-1)) {
-                return s.substring(nextStartEndIndex[0],nextStartEndIndex[1]+1);
-            }
-            if((nextStartEndIndex[1]-nextStartEndIndex[0])<(minRange[1]-minRange[0])){
-                minRange = nextStartEndIndex;
-            }
-            startIndex = nextStartEndIndex[0];
-          //  endIndex = nextStartEndIndex[1];
-            rightLetter = s.charAt(startIndex);
-            endIndex++;
-
         }
-        System.err.println(System.currentTimeMillis() - start);
-        System.err.println("Iterations: " + count);
-        return s.substring(minRange[0],minRange[1]+1);
+        for (int i = leftIndex; i <=rightIndex ; i++) {
+            if(wordMapStore.has(s.charAt(i)) && !wordMapStore.remove(s.charAt(i))){
+                    leftIndex = i;
+                    break;
+            }
+        }
+        return new int[] {leftIndex, rightIndex};
     }
 
-    public int [] findWindow(String s, int startIndex, int endIndex, String  t, int deff []) {
 
 
 
-        StringBuilder tTmp = new StringBuilder(t);
-        int count = tTmp.length();
+    public int[] findWindow(String s, int startIndex, int endIndex, String t, int deff[]) {
+
+        if (!containsAll(s.substring(startIndex, endIndex + 1), t)) {
+            return deff;
+        }
+
+        StringBuilder sTmp = new StringBuilder(s);
+        int count = t.length();
         int leftIndex = Integer.MAX_VALUE;
         int rightIndex = Integer.MIN_VALUE;
 
-        for (int i = startIndex; i <=endIndex ; i++) {
-            String letter = ""+s.charAt(i);
 
-            int tTmpIndex = tTmp.indexOf(letter);
+        for (int i = 0; i < t.length(); i++) {
 
+            // can't find
+            if (t.length() - i < count) {
+                break;
+            }
+            String letter = "" + t.charAt(i);
+            int tTmpIndex = sTmp.indexOf(letter, startIndex);
 
-                 // found letter in the word
-            if(tTmpIndex!=-1){
+            if (tTmpIndex > endIndex) {
+                break;
+            }
+
+            // found letter in the word
+            if (tTmpIndex != -1) {
                 count--;
-                tTmp.setCharAt(tTmpIndex,'_');
-                leftIndex = Math.min(leftIndex, i);
-                rightIndex = Math.max(rightIndex, i);
+                sTmp.setCharAt(tTmpIndex, '_');
+
+                leftIndex = Math.min(tTmpIndex, leftIndex);
+                rightIndex = Math.max(tTmpIndex, rightIndex);
             }
             // finded all letters
-            if(count==0) {
+            if (count == 0) {
                 break;
             }
         }
 
-        return count == 0? new int[] {leftIndex, rightIndex} : deff;
+        return count == 0 ? new int[]{leftIndex, rightIndex} : deff;
     }
 
     private boolean containsAll(String s, String t) {
@@ -155,16 +212,16 @@ public class Task76Solution {
         return s.substring(beginIndex, endIndex + 1);
     }
 
-    int [] trimRange(String s, int startIndex, int endIndex, String  t, int deff [], boolean trimRangeLeft, boolean trimRangeRight) {
-        int leftIndex  = startIndex;
-        int rightIndex  = endIndex;
-        if(trimRangeLeft) {
+    int[] trimRange(String s, int startIndex, int endIndex, String t, int deff[], boolean trimRangeLeft, boolean trimRangeRight) {
+        int leftIndex = startIndex;
+        int rightIndex = endIndex;
+        if (trimRangeLeft) {
             while (findWindow(s, leftIndex + 1, rightIndex, t, deff) != deff) {
                 leftIndex++;
             }
         }
 
-        if(trimRangeRight) {
+        if (trimRangeRight) {
             while (findWindow(s, leftIndex, rightIndex - 1, t, deff) != deff) {
                 rightIndex--;
             }
@@ -173,5 +230,66 @@ public class Task76Solution {
         return new int[]{leftIndex, rightIndex};
     }
 
+    private static class WordMapStore {
+        Map<Character, int[]> wordMap;
+
+        public WordMapStore(String word) {
+            this.wordMap = convertTo(word);
+        }
+
+        private Map<Character, int[]> convertTo(String t) {
+            Map<Character, int[]> characterMap = new TreeMap<>();
+            for (int i = 0; i < t.length(); i++) {
+                if (!characterMap.containsKey(t.charAt(i))) {
+                    characterMap.put(t.charAt(i), new int[2]); //int[0] - need letters; int[1]-found letters
+                }
+                characterMap.get(t.charAt(i))[0]++; //increment founded character;
+            }
+            return characterMap;
+        }
+
+        void add(char c) {
+            if (this.wordMap.containsKey(c)) {
+                this.wordMap.get(c)[1]++;
+            }
+        }
+
+        boolean remove(char c) {
+            if (canRemove(c)) {
+                if (this.wordMap.get(c)[1] > 0) {
+                    this.wordMap.get(c)[1]--;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        boolean isFull() {
+            long count = wordMap.values().stream().filter(arr -> arr[1] >= arr[0]).count();
+            return count == wordMap.values().size();
+        }
+
+        int length() {
+            int length = 0;
+            for (int[] value : wordMap.values()) {
+                length += value[1];
+            }
+            return length;
+        }
+
+        private boolean canRemove(char c) {
+            if (!this.wordMap.containsKey(c)) {
+                return false;
+            }
+            int[] ints = this.wordMap.get(c);
+            return ints[1] > ints[0];
+        }
+
+        public boolean has(char c) {
+            return this.wordMap.containsKey(c);
+        }
+
+    }
 }
 
--- src/test/java/com/home/task76/Task76SolutionTest.java
+++ src/test/java/com/home/task76/Task76SolutionTest.java
@@ -3,6 +3,12 @@ package com.home.task76;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -56,19 +62,116 @@ public class Task76SolutionTest {
     }
 
     @Test
+    public void test6() {
+        String s = "a";
+        String t =  "aa";
+        String result =  "";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
     public void test7() {
-        String s = "obzcopzocynyrsgsarijyxnkpnukkrvzuwdjldxndmnvevpgmxrmvfwkutwekrffnloyqnntbdohyfqndhzyoykiripdzwiojyoznbtogjyfpouuxvumtewmmnqnkadvzrvouqfbbdiqremqzgevkbhyoznacqwbhtrcjwfkzpdstpjswnpiqxjhywjanhdwavajrhwtwzlrqwmombxcaijzevbtcfsdcuovckoalcseaesmhrrizcjgxkbartdtotpsefsrjmvksqyahpijsrppdqpvmuocofuunonybjivbjviyftsyiicbzxnwnrmvlgkzticetyfcvqcbjvbufdxgcmesdqnowzpshuwcseenwjqhgsdlxatamysrohfnixfprdsljyyfhrnnjsagtuihuczilgvtfcjwgdhpbixlzmakebszxbhrdibpoxiwztshwczamwnninzmqrmpsviydkptjzpktksrortapgpxwojofxeasoyvyprjoguhqobehugwdvtzlenrcttuitsiijswpogicjolfxhiscjggzzissfcnxnvgftxvbfzkukqrtalvktdjsodmtgzqtuyaqvvrbuexgwqzwduixzrpnvegddyyywaquxjxrnuzlmyipuqotkghfkpknqinoidifnfyczzonxydtqroazxhjnrxfbmtlqcsfhshjrxwqvblovaouxwempdrrplefnxmwrwfjtebrfnfanvvmtbzjesctdgbsfnpxlwihalyiafincfcwgdfkvhebphtxukwgjgplrntsuchyjjuqozakiglangxkttsczhnswjksnuqwflmumpexxrznzwxurrysaokwxxqkrggytvsgkyfjrewrcvntomnoazmzycjrjrqemimyhriyxgrzcfuqtjhvjtuhwfzhwpljzajitrhryaqchnuawbxhxrpvyqcvhpggrpplhychyulijhkglinibedauhvdydkqszdbzfkzbvhldstocgydnbfjkcnkfxcyyfbzmmyojgzmasccaahpdnzproaxnexnkamwmkmwslksfpwirexxtymkmojztgmfhydvlqtddewjvsrmyqjrpycbmndhupmdqqabiuelacuvxnhxgtpvrtwfgzpcrbhhtikbcqpctlxszgpfbgcsbaaiapmtsucocmpecgixshrrnhyrpalralbccnxvjzjllarqhznzghswqsnfuyywmzbopyjyauknxddgdthlabjqtwxpxwljvoxkpjjpfvccyikbbrpdsyvlxscuoofkecwtnfkvcnzbxkeabtdusyhrqklhaqreupakxkfzxgawqfwsaboszvlshwzhosojjotgyagygguzntrouhiweuomqptfjjqsxlbylhwtpssdlltgubczxslqjgxuqnmpynnlwjgmebrpokxjnbiltvbebyytnnjlcwyzignmhedwqbfdepqakrelrdfesqrumptwwgifmmbepiktxavhuavlfaqxqhreznbvvlakzeoomykkzftthoemqwliednfsqcnbexbimrvkdhllcesrlhhjsspvfupxwdybablotibypmjutclgjurbmhztboqatrdwsomnxnmocvixxvfiqwmednahdqhxjkvcyhpxxdmzzuyyqdjibvmfkmonfxmohhshpkhmntnoplphqyprveyfsmsxjfosmicdsjrieeytpnbhlsziwxnpmgoxneqbnufhfwrjbqcsdfarybzwaplmxckkgclvwqdbpumsmqkswmjwnkuqbicykoisqwoootrdpdvcuiuswfqmrkctsgrevcxnyncmivsxbpbxzxpwchiwtkroqisnmrbmefbmatmdknaklpgpyqlsccgunaibsloyqpnsibwuowebomrmcegejozypjzjunjmeygozcjqbnrpakdermjcckartbcppmbtkhkmmtcngteigjnxxyzaibtdcwutkvpwezisskfaeljmxyjwykwglqlnofhycwuivdbnpintuyhtyqpwaoelgpbuwiuyeqhbvkqlsfgmeoheexbhnhutxvnvfjwlzfmvpcghiowocdsjcvqrdmkcizxnivbianfpsnzabxqecinhgfyjrjlbikrrgsbgfgyxtzzwwpayapfgueroncpxogouyrdgzdfucfrywtywjeefkvtzxlwmrniselyeodysirqflpduvibfdvedgcrzpzrunpadvawfsmmddqzaaahfxlifobffbyzqqbtlcpquedzjvykvarayfldvmkapjcfzfbmhscdwhciecsbdledspgpdtsteuafzbrjuvmsfrajtulwirzagiqjdiehefmfifocadxfuxrpsemavncdxuoaetjkavqicgndjkkfhbvbhjdcygfwcwyhpirrfjziqonbyxhibelinpllxsjzoiifscwzlyjdmwhnuovvugfhvquuleuzmehggdfubpzolgbhwyeqekzccuypaspozwuhbzbdqdtejuniuuyagackubauvriwneeqfhtwkocuipcelcfrcjcymcuktegiikyosumeioatfcxrheklookaqekljtvtdwhxsteajevpjviqzudnjnqbucnfvkybggaybebljwcstmktgnipdyrxbgewqczzkaxmeazpzbjsntltjwlmuclxirwytvxgvxscztryubtjweehapvxrguzzsatozzjytnamfyiitreyxmanhzeqwgpoikcjlokebksgkaqetverjegqgkicsyqcktmwjwakivtsxjwrgakphqincqrxqhzbcnxljzwturmsaklhnvyungjrxaonjqomdnxpnvihmwzphkyuhwqwdboabepmwgyatyrgtboiypxfavbjtrgwswyvcqhzwibpisydtmltbkydhznbsvxktyfxopwkxzbftzknnwipghuoijrbgqnzovxckvojvsqqraffwowfvqvfcmiicwitrhxdeombgesxexedlakitfovtydxunqnwqqdeeekiwjnwoshqcsljiicgobbbuqakjdonjawgjlezdnqhfdqnmsuavxdpnfzwipmspiabveaarshzwxmirgkmfncvtdrdvfxkpxlkdokxgtwcskmjryyymcthfnkasinihaunohkxaibtsqelockaefjmsuolebtnepauwmrxutspjwaxbmahsjtkfkxlnszribmeofbkyvbjscjtqjakuwvcgunvnonvqbbggfshauqsyznokqbhowjusypfnecffenojfvlblgzntqzlrgzprvhqnpfrrkzxznieiuivajivzijsqijigtatifmbplzqahuidegfoobpymkputzamzvweiyvvzlwihgmmmrcburbgbsdxrfjsbiylitghgcpqjbevvgypxcybubyoijijrhuzcdijfybqbfowlookqmlnplbxvjjosfqviygqyhgamuwzjklbyzopkrnhbywtfoqomweldmlrhjqswctubiknzzvcztyehouvnyiqnvkufaobehxhrjvtisxjlxoumipzjarwvbsaegdkpbsjmpevjbewzuqnfhoohhmdjgfpmjzdmtmykqvtucptwfidpwtwffzolffzqfdearclkyeecuzabjeqhxpmfodsvisnpxrqowdawheydfyhoexvcmihdlzavtqlshdhdgjzpozvvackebhgqppvcrvymljfvooauxcjnbejdivikcoaugxwzsulgfqdtefpehbrlhaoqxwcancuvbqutnfbuygoemditeagmcveatgaikwflozgdhkyfqmjcruyyuemwbqwxyyfiwnvlmbovlmccaoguieu"+
-        "cjgamyzjwxrgwedhsexosmswogckohesskteksqgrjonnrwhywxqkqmywqjlxnfrayykqotkzhxmbwvzstrcjfchvluvbaobymlrcgbbqaprwlsqglsrqvynitklvzmvlamqipryqjpmwhdcsxtkutyfoiqljfhxftnnjgmbpdplnuphuksoestuckgopnlwiyltezuwdmhsgzzajtrpnkkswsglhrjprxlvwftbtdtacvclotdcepuahcootzfkwqhtydwrgqrilwvbpadvpzwybmowluikmsfkvbebrxletigjjlealczoqnnejvowptikumnokysfjyoskvsxztnqhcwsamopfzablnrxokdxktrwqjvqfjimneenqvdxufahsshiemfofwlyiionrybfchuucxtyctixlpfrbngiltgtbwivujcyrwutwnuajcxwtfowuuefpnzqljnitpgkobfkqzkzdkwwpksjgzqvoplbzzjuqqgetlojnblslhpatjlzkbuathcuilqzdwfyhwkwxvpicgkxrxweaqevziriwhjzdqanmkljfatjifgaccefukavvsfrbqshhswtchfjkausgaukeapanswimbznstubmswqadckewemzbwdbogogcysfxhzreafwxxwczigwpuvqtathgkpkijqiqrzwugtr";
-        String t =  "xxtymkmojztgmfhydvlqtddewjvsrmyqjrpycbmndhupmdqq";
-        String result = "xxtymkmojztgmfhydvlqtddewjvsrmyqjrpycbmndhupmdqq"; //aefgc
+        String s = "bba";
+        String t =  "ab";
+        String result =  "ba";
         assertThat(solution.minWindow(s, t), is(equalTo(result)));
     }
 
     @Test
     public void test8() {
-        String s = "a";
-        String t =  "aa";
+        String s = "bba";
+        String t =  "ab";
+        String result =  "ba";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+     @Test
+    public void test9() {
+        String s = "babbBba";
+        String t =  "Abbb";
         String result =  "";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test10() {
+        String s = "bdab";
+        String t =  "ab";
+        String result =  "ab";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test11() {
+        String s = "aaflslflsldkalskaaa";
+        String t =  "aaa";
+        String result =  "aaa";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test13() {
+        String s = "babb";
+        String t =  "baba";
+        String result =  "";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test14() {
+        String s = "bbaac";
+        String t =  "aba";
+        String result =  "baa";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test15() {
+        String s = "aaaaaaaaaaaabbbbbcdd";
+        String t =  "abcdd";
+        String result =  "abbbbbcdd";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test16() {
+        String s = "abcabdebac";
+        String t =  "cda";
+        String result =  "cabd";
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+    @Test
+    public void test20() {
+        String s = readLineByLineJava8("E:\\LeetcodeGit\\src\\test\\java\\com\\home\\task76\\input").trim();
+
+        String t =  readLineByLineJava8("E:\\LeetcodeGit\\src\\test\\java\\com\\home\\task76\\word").trim();
+        String result =  readLineByLineJava8("E:\\LeetcodeGit\\src\\test\\java\\com\\home\\task76\\result")             .trim();
+        assertThat(solution.minWindow(s, t), is(equalTo(result)));
+    }
+
+
+    private static String readLineByLineJava8(String filePath)
+    {
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
+        {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        return contentBuilder.toString();
+    }
+
+    @Test
+    public void test21() {
+        String s = "obzcopzocynyrsgsarijyxnkpnukkrvzuwdjldxndmnvevpgmxrmvfwkutwekrffnloyqnntbdohyfqndhzyoykiripdzwiojyoznbtogjyfpouuxvumtewmmnqnkadvzrvouqfbbdiqremqzgevkbhyoznacqwbhtrcjwfkzpdstpjswnpiqxjhywjanhdwavajrhwtwzlrqwmombxcaijzevbtcfsdcuovckoalcseaesmhrrizcjgxkbartdtotpsefsrjmvksqyahpijsrppdqpvmuocofuunonybjivbjviyftsyiicbzxnwnrmvlgkzticetyfcvqcbjvbufdxgcmesdqnowzpshuwcseenwjqhgsdlxatamysrohfnixfprdsljyyfhrnnjsagtuihuczilgvtfcjwgdhpbixlzmakebszxbhrdibpoxiwztshwczamwnninzmqrmpsviydkptjzpktksrortapgpxwojofxeasoyvyprjoguhqobehugwdvtzlenrcttuitsiijswpogicjolfxhiscjggzzissfcnxnvgftxvbfzkukqrtalvktdjsodmtgzqtuyaqvvrbuexgwqzwduixzrpnvegddyyywaquxjxrnuzlmyipuqotkghfkpknqinoidifnfyczzonxydtqroazxhjnrxfbmtlqcsfhshjrxwqvblovaouxwempdrrplefnxmwrwfjtebrfnfanvvmtbzjesctdgbsfnpxlwihalyiafincfcwgdfkvhebphtxukwgjgplrntsuchyjjuqozakiglangxkttsczhnswjksnuqwflmumpexxrznzwxurrysaokwxxqkrggytvsgkyfjrewrcvntomnoazmzycjrjrqemimyhriyxgrzcfuqtjhvjtuhwfzhwpljzajitrhryaqchnuawbxhxrpvyqcvhpggrpplhychyulijhkglinibedauhvdydkqszdbzfkzbvhldstocgydnbfjkcnkfxcyyfbzmmyojgzmasccaahpdnzproaxnexnkamwmkmwslksfpwirexxtymkmojztgmfhydvlqtddewjvsrmyqjrpycbmndhupmdqqabiuelacuvxnhxgtpvrtwfgzpcrbhhtikbcqpctlxszgpfbgcsbaaiapmtsucocmpecgixshrrnhyrpalralbccnxvjzjllarqhznzghswqsnfuyywmzbopyjyauknxddgdthlabjqtwxpxwljvoxkpjjpfvccyikbbrpdsyvlxscuoofkecwtnfkvcnzbxkeabtdusyhrqklhaqreupakxkfzxgawqfwsaboszvlshwzhosojjotgyagygguzntrouhiweuomqptfjjqsxlbylhwtpssdlltgubczxslqjgxuqnmpynnlwjgmebrpokxjnbiltvbebyytnnjlcwyzignmhedwqbfdepqakrelrdfesqrumptwwgifmmbepiktxavhuavlfaqxqhreznbvvlakzeoomykkzftthoemqwliednfsqcnbexbimrvkdhllcesrlhhjsspvfupxwdybablotibypmjutclgjurbmhztboqatrdwsomnxnmocvixxvfiqwmednahdqhxjkvcyhpxxdmzzuyyqdjibvmfkmonfxmohhshpkhmntnoplphqyprveyfsmsxjfosmicdsjrieeytpnbhlsziwxnpmgoxneqbnufhfwrjbqcsdfarybzwaplmxckkgclvwqdbpumsmqkswmjwnkuqbicykoisqwoootrdpdvcuiuswfqmrkctsgrevcxnyncmivsxbpbxzxpwchiwtkroqisnmrbmefbmatmdknaklpgpyqlsccgunaibsloyqpnsibwuowebomrmcegejozypjzjunjmeygozcjqbnrpakdermjcckartbcppmbtkhkmmtcngteigjnxxyzaibtdcwutkvpwezisskfaeljmxyjwykwglqlnofhycwuivdbnpintuyhtyqpwaoelgpbuwiuyeqhbvkqlsfgmeoheexbhnhutxvnvfjwlzfmvpcghiowocdsjcvqrdmkcizxnivbianfpsnzabxqecinhgfyjrjlbikrrgsbgfgyxtzzwwpayapfgueroncpxogouyrdgzdfucfrywtywjeefkvtzxlwmrniselyeodysirqflpduvibfdvedgcrzpzrunpadvawfsmmddqzaaahfxlifobffbyzqqbtlcpquedzjvykvarayfldvmkapjcfzfbmhscdwhciecsbdledspgpdtsteuafzbrjuvmsfrajtulwirzagiqjdiehefmfifocadxfuxrpsemavncdxuoaetjkavqicgndjkkfhbvbhjdcygfwcwyhpirrfjziqonbyxhibelinpllxsjzoiifscwzlyjdmwhnuovvugfhvquuleuzmehggdfubpzolgbhwyeqekzccuypaspozwuhbzbdqdtejuniuuyagackubauvriwneeqfhtwkocuipcelcfrcjcymcuktegiikyosumeioatfcxrheklookaqekljtvtdwhxsteajevpjviqzudnjnqbucnfvkybggaybebljwcstmktgnipdyrxbgewqczzkaxmeazpzbjsntltjwlmuclxirwytvxgvxscztryubtjweehapvxrguzzsatozzjytnamfyiitreyxmanhzeqwgpoikcjlokebksgkaqetverjegqgkicsyqcktmwjwakivtsxjwrgakphqincqrxqhzbcnxljzwturmsaklhnvyungjrxaonjqomdnxpnvihmwzphkyuhwqwdboabepmwgyatyrgtboiypxfavbjtrgwswyvcqhzwibpisydtmltbkydhznbsvxktyfxopwkxzbftzknnwipghuoijrbgqnzovxckvojvsqqraffwowfvqvfcmiicwitrhxdeombgesxexedlakitfovtydxunqnwqqdeeekiwjnwoshqcsljiicgobbbuqakjdonjawgjlezdnqhfdqnmsuavxdpnfzwipmspiabveaarshzwxmirgkmfncvtdrdvfxkpxlkdokxgtwcskmjryyymcthfnkasinihaunohkxaibtsqelockaefjmsuolebtnepauwmrxutspjwaxbmahsjtkfkxlnszribmeofbkyvbjscjtqjakuwvcgunvnonvqbbggfshauqsyznokqbhowjusypfnecffenojfvlblgzntqzlrgzprvhqnpfrrkzxznieiuivajivzijsqijigtatifmbplzqahuidegfoobpymkputzamzvweiyvvzlwihgmmmrcburbgbsdxrfjsbiylitghgcpqjbevvgypxcybubyoijijrhuzcdijfybqbfowlookqmlnplbxvjjosfqviygqyhgamuwzjklbyzopkrnhbywtfoqomweldmlrhjqswctubiknzzvcztyehouvnyiqnvkufaobehxhrjvtisxjlxoumipzjarwvbsaegdkpbsjmpevjbewzuqnfhoohhmdjgfpmjzdmtmykqvtucptwfidpwtwffzolffzqfdearclkyeecuzabjeqhxpmfodsvisnpxrqowdawheydfyhoexvcmihdlzavtqlshdhdgjzpozvvackebhgqppvcrvymljfvooauxcjnbejdivikcoaugxwzsulgfqdtefpehbrlhaoqxwcancuvbqutnfbuygoemditeagmcveatgaikwflozgdhkyfqmjcruyyuemwbqwxyyfiwnvlmbovlmccaoguieu";
+        String t =  "cjgamyzjwxrgwedhsexosmswogckohesskteksqgrjonnrwhywxqkqmywqjlxnfrayykqotkzhxmbwvzstrcjfchvluvbaobymlrcgbbqaprwlsqglsrqvynitklvzmvlamqipryqjpmwhdcsxtkutyfoiqljfhxftnnjgmbpdplnuphuksoestuckgopnlwiyltezuwdmhsgzzajtrpnkkswsglhrjprxlvwftbtdtacvclotdcepuahcootzfkwqhtydwrgqrilwvbpadvpzwybmowluikmsfkvbebrxletigjjlealczoqnnejvowptikumnokysfjyoskvsxztnqhcwsamopfzablnrxokdxktrwqjvqfjimneenqvdxufahsshiemfofwlyiionrybfchuucxtyctixlpfrbngiltgtbwivujcyrwutwnuajcxwtfowuuefpnzqljnitpgkobfkqzkzdkwwpksjgzqvoplbzzjuqqgetlojnblslhpatjlzkbuathcuilqzdwfyhwkwxvpicgkxrxweaqevziriwhjzdqanmkljfatjifgaccefukavvsfrbqshhswtchfjkausgaukeapanswimbznstubmswqadckewemzbwdbogogcysfxhzreafwxxwczigwpuvqtathgkpkijqiqrzwugtr";
+        String result =  "twxpxwljvoxkpjjpfvccyikbbrpdsyvlxscuoofkecwtnfkvcnzbxkeabtdusyhrqklhaqreupakxkfzxgawqfwsaboszvlshwzhosojjotgyagygguzntrouhiweuomqptfjjqsxlbylhwtpssdlltgubczxslqjgxuqnmpynnlwjgmebrpokxjnbiltvbebyytnnjlcwyzignmhedwqbfdepqakrelrdfesqrumptwwgifmmbepiktxavhuavlfaqxqhreznbvvlakzeoomykkzftthoemqwliednfsqcnbexbimrvkdhllcesrlhhjsspvfupxwdybablotibypmjutclgjurbmhztboqatrdwsomnxnmocvixxvfiqwmednahdqhxjkvcyhpxxdmzzuyyqdjibvmfkmonfxmohhshpkhmntnoplphqyprveyfsmsxjfosmicdsjrieeytpnbhlsziwxnpmgoxneqbnufhfwrjbqcsdfarybzwaplmxckkgclvwqdbpumsmqkswmjwnkuqbicykoisqwoootrdpdvcuiuswfqmrkctsgrevcxnyncmivsxbpbxzxpwchiwtkroqisnmrbmefbmatmdknaklpgpyqlsccgunaibsloyqpnsibwuowebomrmcegejozypjzjunjmeygozcjqbnrpakdermjcckartbcppmbtkhkmmtcngteigjnxxyzaibtdcwutkvpwezisskfaeljmxyjwykwglqlnofhycwuivdbnpintuyhtyqpwaoelgpbuwiuyeqhbvkqlsfgmeoheexbhnhutxvnvfjwlzfmvpcghiowocdsjcvqrdmkcizxnivbianfpsnzabxqecinhgfyjrjlbikrrgsbgfgyxtzzwwpayapfgueroncpxogouyrdgzdfucfrywtywjeefkvtzxlw";
         assertThat(solution.minWindow(s, t), is(equalTo(result)));
     }
 }
